fb3c386fbdc2fa4a81d4f0f2762a4e6f4eb38767
donghwa <donghwa.dev@gmail.com>
2026-02-15 17:08:45 +0900

    fix-port

--- src/index.js
+++ src/index.js
@@ -37,6 +37,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Internal Server Error' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
